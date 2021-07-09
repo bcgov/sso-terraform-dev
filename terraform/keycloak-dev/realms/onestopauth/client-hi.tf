0e379099f4cad594ff8f22e60abf13aeff092a23
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-07-08 17:11:20 -0700

    feat: add a client file for hi

--- terraform/keycloak-dev/realms/onestopauth/client-hi.tf
+++ terraform/keycloak-dev/realms/onestopauth/client-hi.tf
@@ -2,4 +2,7 @@ module "client_hi" {
   source      = "../../../modules/openid-client"
   realm_id    = data.keycloak_realm.this.id
   client_name = "hi"
+  valid_redirect_uris = [
+    "http://example.com"
+  ]
 }
