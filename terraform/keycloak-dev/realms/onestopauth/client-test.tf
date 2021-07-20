0b434d4c978528266474f65492eba62d42104a3b
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-07-20 15:39:14 -0700

    feat: add a client file for test

--- terraform/keycloak-dev/realms/onestopauth/client-test.tf
+++ terraform/keycloak-dev/realms/onestopauth/client-test.tf
@@ -3,6 +3,11 @@ module "client_test" {
   realm_id    = data.keycloak_realm.this.id
   client_name = "test"
   valid_redirect_uris = [
-    "http://example.com"
+    "http://dev.com"
+  ]
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
   ]
 }
