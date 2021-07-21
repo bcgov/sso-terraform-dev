26153c1efbca000090d4486f585461c1d2c4aa76
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-07-21 16:57:30 -0700

    feat: add a client file for junmin-client-test-101

--- terraform/keycloak-prod/realms/onestopauth/client-junmin-client-test-101.tf
+++ terraform/keycloak-prod/realms/onestopauth/client-junmin-client-test-101.tf
@@ -3,6 +3,11 @@ module "client_junmin-client-test-101" {
   realm_id    = data.keycloak_realm.this.id
   client_name = "junmin-client-test-101"
   valid_redirect_uris = [
-    "http://localhost:3000"
+    "http://localhost:3003"
+  ]
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
   ]
 }
