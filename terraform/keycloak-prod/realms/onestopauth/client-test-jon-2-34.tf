d4947a8cc24958e4b5e90f1f259bb89dbb799075
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-07-26 16:18:35 -0700

    feat: add a client file for test-jon-2-34

---
+++ terraform/keycloak-prod/realms/onestopauth/client-test-jon-2-34.tf
@@ -0,0 +1,8 @@
+module "client_test-jon-2-34" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "test-jon-2-34"
+  valid_redirect_uris = [
+    "http://prod"
+  ]
+}
