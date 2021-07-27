5ab29489ec66b160f327f8b99183ddb27e4ee40e
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-07-26 17:12:00 -0700

    feat: add a client file for demo-1-38

---
+++ terraform/keycloak-dev/realms/onestopauth/client-demo-1-38.tf
@@ -0,0 +1,9 @@
+module "client_demo-1-38" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "demo-1-38"
+  valid_redirect_uris = [
+    "http://dev",
+    "http://dev2"
+  ]
+}
