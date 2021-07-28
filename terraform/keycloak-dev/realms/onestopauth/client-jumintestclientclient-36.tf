28f7999cdd8e7e0597e1513d1a1d5e1c4e91d841
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-07-27 17:28:18 -0700

    feat: add a client file for jumintestclientclient-36

---
+++ terraform/keycloak-dev/realms/onestopauth/client-jumintestclientclient-36.tf
@@ -0,0 +1,9 @@
+module "client_jumintestclientclient-36" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "jumintestclientclient-36"
+  valid_redirect_uris = [
+    "http://localhost:3000",
+    "http://localhost:3001"
+  ]
+}
