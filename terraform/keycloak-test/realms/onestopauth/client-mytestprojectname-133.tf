023dc56345ae32947a9c3b7bdfaf67294d3103aa
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-07-28 22:53:13 -0700

    chore: remove a client file for mytestprojectname-133

--- terraform/keycloak-test/realms/onestopauth/client-mytestprojectname-133.tf
+++
@@ -1,8 +0,0 @@
-module "client_mytestprojectname-133" {
-  source      = "../../../modules/openid-client"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "mytestprojectname-133"
-  valid_redirect_uris = [
-    "http://localhost:3000"
-  ]
-}
