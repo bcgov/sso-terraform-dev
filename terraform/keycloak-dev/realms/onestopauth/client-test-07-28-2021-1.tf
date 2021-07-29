990dcb3de494a07aca48aafa99c0d6d74f67f196
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-07-29 08:36:32 -0700

    feat: add a client file for test-07-28-2021-1

---
+++ terraform/keycloak-dev/realms/onestopauth/client-test-07-28-2021-1.tf
@@ -0,0 +1,9 @@
+module "client_test-07-28-2021-1" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "test-07-28-2021-1"
+  valid_redirect_uris = [
+    "http://tr",
+    "http://trss"
+  ]
+}
