f546a807e28861af8c7b9db7cd51f93ddb65ecce
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-07-29 18:56:44 -0700

    feat: add a client file for negajjangproject-1

---
+++ terraform/keycloak-dev/realms/onestopauth/client-negajjangproject-1.tf
@@ -0,0 +1,8 @@
+module "client_negajjangproject-1" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "negajjangproject-1"
+  valid_redirect_uris = [
+    "http://localhosthhh"
+  ]
+}
