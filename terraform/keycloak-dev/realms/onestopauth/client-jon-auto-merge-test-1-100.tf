c22b319feae24ac589290fc92abd68f6012c6a4f
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-07-30 14:56:26 -0700

    feat: add a client file for jon-auto-merge-test-1-100

---
+++ terraform/keycloak-dev/realms/onestopauth/client-jon-auto-merge-test-1-100.tf
@@ -0,0 +1,13 @@
+module "client_jon-auto-merge-test-1-100" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "jon-auto-merge-test-1-100"
+  valid_redirect_uris = [
+    "http://a"
+  ]
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
