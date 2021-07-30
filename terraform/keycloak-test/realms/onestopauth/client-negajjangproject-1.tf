763a8a73af3a652cad41f8826a726684066603c3
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-07-29 18:56:43 -0700

    chore: remove a client file for negajjangproject-1

--- terraform/keycloak-test/realms/onestopauth/client-negajjangproject-1.tf
+++
@@ -1,8 +0,0 @@
-module "client_negajjangproject-1" {
-  source      = "../../../modules/openid-client"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "negajjangproject-1"
-  valid_redirect_uris = [
-    "http://localhost/"
-  ]
-}
