0862d1c0080834ebab1dcefa919e885ad11fc4c7
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-07-30 12:43:28 -0700

    feat: add a client file for testprojectjunmin-2222-aaaa-35

---
+++ terraform/keycloak-test/realms/onestopauth/client-testprojectjunmin-2222-aaaa-35.tf
@@ -0,0 +1,13 @@
+module "client_testprojectjunmin-2222-aaaa-35" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "testprojectjunmin-2222-aaaa-35"
+  valid_redirect_uris = [
+    "http://localhost:1000"
+  ]
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
