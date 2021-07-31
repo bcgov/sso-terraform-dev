13738f02387eac432488d041dd2131e11b00e4ca
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-07-30 17:19:52 -0700

    feat: add a client file for automergetestproject-133

---
+++ terraform/keycloak-prod/realms/onestopauth/client-automergetestproject-133.tf
@@ -0,0 +1,13 @@
+module "client_automergetestproject-133" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "automergetestproject-133"
+  valid_redirect_uris = [
+    "http://locahost/"
+  ]
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
