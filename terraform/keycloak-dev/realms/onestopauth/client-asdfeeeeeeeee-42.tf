470ead1c0f007a222dd307882d07c8a6d00b4103
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-08-05 15:04:46 -0700

    feat: add a client file for asdfeeeeeeeee-42

---
+++ terraform/keycloak-dev/realms/onestopauth/client-asdfeeeeeeeee-42.tf
@@ -0,0 +1,8 @@
+module "client_asdfeeeeeeeee-42" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "asdfeeeeeeeee-42"
+  valid_redirect_uris = [
+    "http://localhost:1222"
+  ]
+}
