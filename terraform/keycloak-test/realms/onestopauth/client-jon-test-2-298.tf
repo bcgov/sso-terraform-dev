3574e38a08c68be692997e1b9032e72cd9a1b0d2
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-08-09 12:04:08 -0700

    feat: add a client file for jon-test-2-298

---
+++ terraform/keycloak-test/realms/onestopauth/client-jon-test-2-298.tf
@@ -0,0 +1,13 @@
+module "client_jon-test-2-298" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "jon-test-2-298"
+  valid_redirect_uris = [
+    "http://stackoverflow.com/questions/58579426/in-useeffect-whats-the-difference-between-providing-no-dependency-array-and-an"
+  ]
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
