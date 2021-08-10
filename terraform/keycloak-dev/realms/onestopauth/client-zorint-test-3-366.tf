f9d2244bf4d2fb95f3d4224899d5b8f6789fe66e
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-08-09 21:08:54 -0700

    feat: add a client file for zorint-test-3-366

---
+++ terraform/keycloak-dev/realms/onestopauth/client-zorint-test-3-366.tf
@@ -0,0 +1,8 @@
+module "client_zorint-test-3-366" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "zorint-test-3-366"
+  valid_redirect_uris = [
+    "https://www.google.ca"
+  ]
+}
