7bf44d13d0d2b4b0628589966744898de96c334b
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-08-10 11:33:56 -0700

    feat: add a client file for j-339

---
+++ terraform/keycloak-prod/realms/onestopauth/client-j-339.tf
@@ -0,0 +1,13 @@
+module "client_j-339" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "j-339"
+  valid_redirect_uris = [
+    "http://as"
+  ]
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
