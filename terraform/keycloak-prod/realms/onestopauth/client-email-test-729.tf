cbc801770c993a860e9c674703e7b9a61bc60cb7
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-08-19 09:10:28 -0700

    feat: add a client file for email-test-729

---
+++ terraform/keycloak-prod/realms/onestopauth/client-email-test-729.tf
@@ -0,0 +1,8 @@
+module "client_email-test-729" {
+  source      = "../../../modules/openid-client"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "email-test-729"
+  valid_redirect_uris = [
+    "http://c"
+  ]
+}
