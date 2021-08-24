f7f9257cde32b272bb3b07054e6d75718e28155a
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-08-24 09:02:05 -0700

    chore: remove a client file for test-958

--- terraform/keycloak-prod/realms/onestopauth/client-test-958.tf
+++
@@ -1,8 +0,0 @@
-module "client_test-958" {
-  source      = "../../../modules/openid-client"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "test-958"
-  valid_redirect_uris = [
-    "http://c"
-  ]
-}
