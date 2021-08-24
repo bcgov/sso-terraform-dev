8bfe389c8d3af155ed1429f1941d04701be30e94
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-08-24 12:26:52 -0700

    chore: remove a client file for asdfaaaaaaaaaa-232

--- terraform/keycloak-dev/realms/onestopauth/client-asdfaaaaaaaaaa-232.tf
+++
@@ -1,8 +0,0 @@
-module "client_asdfaaaaaaaaaa-232" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "asdfaaaaaaaaaa-232"
-  valid_redirect_uris = [
-    "http://localhost:1000"
-  ]
-}
