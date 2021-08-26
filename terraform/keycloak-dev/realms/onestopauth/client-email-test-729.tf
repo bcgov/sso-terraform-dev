5f42b732e039fb4caf613c5eda47b990fa3a6680
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-08-26 09:31:10 -0700

    chore: remove a client file for email-test-729

--- terraform/keycloak-dev/realms/onestopauth/client-email-test-729.tf
+++
@@ -1,8 +0,0 @@
-module "client_email-test-729" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "email-test-729"
-  valid_redirect_uris = [
-    "http://a"
-  ]
-}
