2e08070c7fabe36108b9039a67f551fbcf02303d
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-08-30 09:14:01 -0700

    feat: add a client file for email-test-jon-1256

---
+++ terraform/keycloak-dev/realms/onestopauth/client-email-test-jon-1256.tf
@@ -0,0 +1,13 @@
+module "client_email-test-jon-1256" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "email-test-jon-1256"
+  valid_redirect_uris = [
+    "http://a"
+  ]
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
