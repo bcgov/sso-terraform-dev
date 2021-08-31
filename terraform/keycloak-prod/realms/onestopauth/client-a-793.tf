5ce0033b534ebf57d13c65e75a1dae95c45237f7
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-08-30 17:01:24 -0700

    chore: remove a client file for a-793

--- terraform/keycloak-prod/realms/onestopauth/client-a-793.tf
+++
@@ -1,8 +0,0 @@
-module "client_a-793" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "a-793"
-  valid_redirect_uris = [
-    "http://c"
-  ]
-}
