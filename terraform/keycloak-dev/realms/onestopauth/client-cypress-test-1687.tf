7268ad413aa4a358dae3548416348c957050b505
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-09-10 08:58:33 -0700

    feat: add a client file for cypress-test-1687

---
+++ terraform/keycloak-dev/realms/onestopauth/client-cypress-test-1687.tf
@@ -0,0 +1,13 @@
+module "client_cypress-test-1687" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "cypress-test-1687"
+  valid_redirect_uris = [
+    "http://cypress"
+  ]
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
