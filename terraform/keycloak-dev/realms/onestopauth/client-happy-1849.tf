e9c97cd0c1cb290ed5227c8ee0bcf2e772020923
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-09-20 13:14:11 -0700

    feat: add a client file for happy-1849

---
+++ terraform/keycloak-dev/realms/onestopauth/client-happy-1849.tf
@@ -0,0 +1,13 @@
+module "client_happy-1849" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "happy-1849"
+  valid_redirect_uris = [
+    "http://a"
+  ]
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
