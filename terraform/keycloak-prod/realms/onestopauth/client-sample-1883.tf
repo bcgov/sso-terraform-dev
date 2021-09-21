16e1cd3962f7aca6de5db5b8edebe8e35b8a45aa
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-09-21 10:22:52 -0700

    feat: add a client file for sample-1883

---
+++ terraform/keycloak-prod/realms/onestopauth/client-sample-1883.tf
@@ -0,0 +1,13 @@
+module "client_sample-1883" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "sample-1883"
+  valid_redirect_uris = [
+    "http://c"
+  ]
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
