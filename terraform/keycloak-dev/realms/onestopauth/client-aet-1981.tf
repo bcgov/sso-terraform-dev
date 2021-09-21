b83dcd33bd3e2c88da2f0e2ffbf9706cb0ba1a66
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-09-21 12:46:30 -0700

    feat: add a client file for aet-1981

---
+++ terraform/keycloak-dev/realms/onestopauth/client-aet-1981.tf
@@ -0,0 +1,13 @@
+module "client_aet-1981" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "aet-1981"
+  valid_redirect_uris = [
+    "http://a"
+  ]
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
