b6987eab94d5c044faad6c7d0050c3d047b5a4fc
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-09-27 13:55:32 -0700

    chore: remove a client file for test-sept-21-2113

--- terraform/keycloak-test/realms/onestopauth/client-test-sept-21-2113.tf
+++
@@ -1,8 +0,0 @@
-module "client_test-sept-21-2113" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "test-sept-21-2113"
-  valid_redirect_uris = [
-    "http://b"
-  ]
-}
