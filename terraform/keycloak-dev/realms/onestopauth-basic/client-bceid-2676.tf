307d8941618c877a8bbfc856f7288d679ccef524
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-11-02 15:17:18 -0700

    feat: add a client file for bceid-2676

---
+++ terraform/keycloak-dev/realms/onestopauth-basic/client-bceid-2676.tf
@@ -0,0 +1,14 @@
+module "client_bceid-2676" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "bceid-2676"
+  valid_redirect_uris = [
+    "http://a"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
