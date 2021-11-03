15013a48cf6b2c861cf4a59beef3508d7d07dce7
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-11-03 09:39:46 -0700

    feat: add a client file for zz-2807

---
+++ terraform/keycloak-test/realms/onestopauth-basic/client-zz-2807.tf
@@ -0,0 +1,9 @@
+module "client_zz-2807" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "zz-2807"
+  valid_redirect_uris = [
+    "https://."
+  ]
+  description = "CSS App Created"
+}
