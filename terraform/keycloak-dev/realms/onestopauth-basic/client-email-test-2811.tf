1ed2cd9a56bdeb842865155e36c1c034b4a98753
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-11-03 12:37:25 -0700

    feat: add a client file for email-test-2811

---
+++ terraform/keycloak-dev/realms/onestopauth-basic/client-email-test-2811.tf
@@ -0,0 +1,14 @@
+module "client_email-test-2811" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "email-test-2811"
+  valid_redirect_uris = [
+    "http://a"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
