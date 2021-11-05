eb543ee67f840cc09a5a9ea075b2d6e01d5bf862
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-11-05 15:53:06 -0700

    feat: add a client file for conf-2-2872

---
+++ terraform/keycloak-dev/realms/onestopauth/client-conf-2-2872.tf
@@ -0,0 +1,11 @@
+module "client_conf-2-2872" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "conf-2-2872"
+  valid_redirect_uris = [
+    "http://a",
+    "http://localhost:3000",
+    "http://localhost:3000/*"
+  ]
+  description = "CSS App Created"
+}
