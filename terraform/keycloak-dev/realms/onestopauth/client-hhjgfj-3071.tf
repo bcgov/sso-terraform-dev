b235616976fa7e314364a53f580f8ca28b8a4b94
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-11-17 10:17:55 -0800

    feat: add a client file for hhjgfj-3071

---
+++ terraform/keycloak-dev/realms/onestopauth/client-hhjgfj-3071.tf
@@ -0,0 +1,14 @@
+module "client_hhjgfj-3071" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "hhjgfj-3071"
+  valid_redirect_uris = [
+    "http://www.google.ca"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
