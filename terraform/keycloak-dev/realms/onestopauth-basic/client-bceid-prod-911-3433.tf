1c33880b39e47209aff4b552f57c55b6e20d6857
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-12-01 10:15:24 -0800

    feat: add a client file for bceid-prod-911-3433

---
+++ terraform/keycloak-dev/realms/onestopauth-basic/client-bceid-prod-911-3433.tf
@@ -0,0 +1,21 @@
+data "keycloak_authentication_flow" "bceid_prod_911_3433_browserflow" {
+  realm_id = data.keycloak_realm.this.id
+  alias    = "null"
+}
+module "client_bceid-prod-911-3433" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "bceid-prod-911-3433"
+  valid_redirect_uris = [
+    "http://dev",
+    "http://dev"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.bceid_prod_911_3433_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "+"
+  ]
+}
