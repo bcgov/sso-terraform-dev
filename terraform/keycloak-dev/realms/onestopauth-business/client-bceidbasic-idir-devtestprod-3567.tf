e5e3a871a0c00a66d006a348ec51e0d953e4bd65
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-12-01 11:18:15 -0800

    feat: add a client file for bceidbasic-idir-devtestprod-3567

---
+++ terraform/keycloak-dev/realms/onestopauth-business/client-bceidbasic-idir-devtestprod-3567.tf
@@ -0,0 +1,10 @@
+module "client_bceidbasic-idir-devtestprod-3567" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "bceidbasic-idir-devtestprod-3567"
+  valid_redirect_uris = [
+    "https:///www.test.ca/dev",
+    "https:///www.test.ca/dev2"
+  ]
+  description = "CSS App Created"
+}
