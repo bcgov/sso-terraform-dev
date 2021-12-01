f0f072afbf1022107d2a5d12c447326bd95ae377
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-12-01 13:10:19 -0800

    feat: add a client file for idir-test-dev-3565

---
+++ terraform/keycloak-test/realms/onestopauth/client-idir-test-dev-3565.tf
@@ -0,0 +1,14 @@
+module "client_idir-test-dev-3565" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "idir-test-dev-3565"
+  valid_redirect_uris = [
+    "https://www.test.ca/testCHANGED"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "+"
+  ]
+}
