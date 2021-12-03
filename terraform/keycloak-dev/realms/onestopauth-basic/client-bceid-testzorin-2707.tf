ac42657220d6e27054687e5a7def47d0b6923174
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-12-03 13:28:21 -0800

    chore: remove a client file for bceid-testzorin-2707

--- terraform/keycloak-dev/realms/onestopauth-basic/client-bceid-testzorin-2707.tf
+++
@@ -1,9 +0,0 @@
-module "client_bceid-testzorin-2707" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "bceid-testzorin-2707"
-  valid_redirect_uris = [
-    "https://www.google.ca"
-  ]
-  description = "CSS App Created"
-}
