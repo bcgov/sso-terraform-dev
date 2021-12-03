5eb9eebd3fecf045a4ac07a1c231d678a4713046
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2021-12-03 13:08:07 -0800

    chore: remove a client file for test-3070

--- terraform/keycloak-test/realms/onestopauth-basic/client-test-3070.tf
+++
@@ -1,9 +0,0 @@
-module "client_test-3070" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "test-3070"
-  valid_redirect_uris = [
-    "https://www.google.ca"
-  ]
-  description = "CSS App Created"
-}
