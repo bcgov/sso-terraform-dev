a7f6776cf0b85d21b32147e39757746759e85ba0
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-02 13:55:13 -0800

    feat: add a client file for sample-project-name-3

---
+++ terraform/keycloak-dev/realms/onestopauth-basic/client-sample-project-name-3.tf
@@ -0,0 +1,15 @@
+module "client_sample-project-name-3" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "sample-project-name-3"
+  valid_redirect_uris = [
+    "http://localhost:3000"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "http://localhost:3000",
+    "+"
+  ]
+}
