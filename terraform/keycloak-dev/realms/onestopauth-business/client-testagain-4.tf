86b7fade35327b0426aefb3941050ebcb7d33a80
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-02 17:15:40 -0800

    feat: add a client file for testagain-4

---
+++ terraform/keycloak-dev/realms/onestopauth-business/client-testagain-4.tf
@@ -0,0 +1,15 @@
+module "client_testagain-4" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "testagain-4"
+  valid_redirect_uris = [
+    "http://localhost:3000"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "http://localhost:3000",
+    "+"
+  ]
+}
