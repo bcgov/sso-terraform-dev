bb686a99e7d4755f1a87d89651fb8787b8e66dbc
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-04 11:45:34 -0800

    feat: add a client file for tians-testing-multiple-members-2-4-4984

---
+++ terraform/keycloak-test/realms/onestopauth-basic/client-tians-testing-multiple-members-2-4-4984.tf
@@ -0,0 +1,15 @@
+module "client_tians-testing-multiple-members-2-4-4984" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "tians-testing-multiple-members-2-4-4984"
+  valid_redirect_uris = [
+    "https://example"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://example",
+    "+"
+  ]
+}
