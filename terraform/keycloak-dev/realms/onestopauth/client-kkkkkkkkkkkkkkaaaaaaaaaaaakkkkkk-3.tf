ec0d99c60559587cff902ba8f774129f3300bf40
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-09 11:22:13 -0800

    feat: add a client file for kkkkkkkkkkkkkkaaaaaaaaaaaakkkkkk-3

---
+++ terraform/keycloak-dev/realms/onestopauth/client-kkkkkkkkkkkkkkaaaaaaaaaaaakkkkkk-3.tf
@@ -0,0 +1,9 @@
+module "client_kkkkkkkkkkkkkkaaaaaaaaaaaakkkkkk-3" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "kkkkkkkkkkkkkkaaaaaaaaaaaakkkkkk-3"
+  valid_redirect_uris = [
+    "http://localhost:3002"
+  ]
+  description = "CSS App Created"
+}
