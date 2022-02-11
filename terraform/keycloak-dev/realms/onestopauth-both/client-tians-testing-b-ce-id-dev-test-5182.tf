26a7ea4fadca37a30bc2726bf9e3d5e33e7a1208
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-10 17:41:05 -0800

    chore: remove a client file for tians-testing-b-ce-id-dev-test-5182

--- terraform/keycloak-dev/realms/onestopauth-both/client-tians-testing-b-ce-id-dev-test-5182.tf
+++
@@ -1,9 +0,0 @@
-module "client_tians-testing-b-ce-id-dev-test-5182" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "tians-testing-b-ce-id-dev-test-5182"
-  valid_redirect_uris = [
-    "https://exampleddddd"
-  ]
-  description = "CSS App Created"
-}
