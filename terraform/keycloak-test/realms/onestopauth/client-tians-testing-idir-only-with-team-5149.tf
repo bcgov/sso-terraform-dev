d2763e497782c870e25bb0df67296cfc728bf01f
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-14 15:12:33 -0800

    feat: add a client file for tians-testing-idir-only-with-team-5149

---
+++ terraform/keycloak-test/realms/onestopauth/client-tians-testing-idir-only-with-team-5149.tf
@@ -0,0 +1,17 @@
+module "client_tians-testing-idir-only-with-team-5149" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "tians-testing-idir-only-with-team-5149"
+  valid_redirect_uris = [
+    "https://example",
+    "https://example"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://example",
+    "https://example",
+    "+"
+  ]
+}
