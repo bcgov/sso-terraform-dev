937fdf1ec58b841bed35072c82420324292ae9f4
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-27 14:38:22 -0800

    feat: add a client file for feb-27-test-2-5744

---
+++ terraform/keycloak-dev/realms/onestopauth-basic/client-feb-27-test-2-5744.tf
@@ -0,0 +1,9 @@
+module "client_feb-27-test-2-5744" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "feb-27-test-2-5744"
+  valid_redirect_uris = [
+    "https:///www.test.ca/devChange"
+  ]
+  description = "CSS App Created"
+}
