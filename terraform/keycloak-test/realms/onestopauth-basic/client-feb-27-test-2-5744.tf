7f85106f973d3a0d0f29d777b674190a996f3b71
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-28 20:57:01 -0800

    chore: remove a client file for feb-27-test-2-5744

--- terraform/keycloak-test/realms/onestopauth-basic/client-feb-27-test-2-5744.tf
+++
@@ -1,9 +0,0 @@
-module "client_feb-27-test-2-5744" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "feb-27-test-2-5744"
-  valid_redirect_uris = [
-    "https://www.test.ca/testCHANGED"
-  ]
-  description = "CSS App Created"
-}
