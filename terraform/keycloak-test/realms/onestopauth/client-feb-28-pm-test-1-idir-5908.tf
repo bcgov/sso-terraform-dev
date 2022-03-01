aad89b4a534cb2bf659e4b5a537454fe87b5e4e0
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-02-28 20:30:19 -0800

    feat: add a client file for feb-28-pm-test-1-idir-5908

---
+++ terraform/keycloak-test/realms/onestopauth/client-feb-28-pm-test-1-idir-5908.tf
@@ -0,0 +1,17 @@
+module "client_feb-28-pm-test-1-idir-5908" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "feb-28-pm-test-1-idir-5908"
+  valid_redirect_uris = [
+    "https://www.test.ca/test",
+    "https://www.test.ca/testCHANGED2"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://www.test.ca/test",
+    "https://www.test.ca/testCHANGED2",
+    "+"
+  ]
+}
