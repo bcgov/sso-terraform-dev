65c850b1f6bac89b42a864653f8ce810309f6736
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-03-02 11:50:01 -0800

    feat: add a client file for march-3-jon-b-ce-id-basic-test-6073

---
+++ terraform/keycloak-dev/realms/onestopauth-basic/client-march-3-jon-b-ce-id-basic-test-6073.tf
@@ -0,0 +1,15 @@
+module "client_march-3-jon-b-ce-id-basic-test-6073" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "march-3-jon-b-ce-id-basic-test-6073"
+  valid_redirect_uris = [
+    "http://bceidDev.com"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "http://bceidDev.com",
+    "+"
+  ]
+}
