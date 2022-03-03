3205893a7891e9e51edc175fbced27ee2670685a
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-03-02 16:08:10 -0800

    feat: add a client file for kuro-mar-02-idir-test-6077

---
+++ terraform/keycloak-test/realms/onestopauth/client-kuro-mar-02-idir-test-6077.tf
@@ -0,0 +1,10 @@
+module "client_kuro-mar-02-idir-test-6077" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "kuro-mar-02-idir-test-6077"
+  valid_redirect_uris = [
+    "https://localhost:3000",
+    "https://www.testditredirect.ca"
+  ]
+  description = "CSS App Created"
+}
