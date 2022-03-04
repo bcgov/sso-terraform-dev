43cf8ef82107311e9f9bf87f58b9e4c2dd28a3d6
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-03-03 16:33:24 -0800

    feat: add a client file for kuro-mar-03-b-ce-id-business-test-6238

---
+++ terraform/keycloak-dev/realms/onestopauth-business/client-kuro-mar-03-b-ce-id-business-test-6238.tf
@@ -0,0 +1,10 @@
+module "client_kuro-mar-03-b-ce-id-business-test-6238" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "kuro-mar-03-b-ce-id-business-test-6238"
+  valid_redirect_uris = [
+    "http://bceidDev.com",
+    "http://devchange.com"
+  ]
+  description = "CSS App Created"
+}
