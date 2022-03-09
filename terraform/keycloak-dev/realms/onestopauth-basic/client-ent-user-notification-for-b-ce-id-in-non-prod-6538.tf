57fd78f0e1fb57e6abf6926c7fd90b4e4eceb882
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-03-09 15:01:20 -0800

    feat: add a client file for ent-user-notification-for-b-ce-id-in-non-prod-6538

---
+++ terraform/keycloak-dev/realms/onestopauth-basic/client-ent-user-notification-for-b-ce-id-in-non-prod-6538.tf
@@ -0,0 +1,15 @@
+module "client_ent-user-notification-for-b-ce-id-in-non-prod-6538" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "ent-user-notification-for-b-ce-id-in-non-prod-6538"
+  valid_redirect_uris = [
+    "http://devchange.com"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "http://devchange.com",
+    "+"
+  ]
+}
