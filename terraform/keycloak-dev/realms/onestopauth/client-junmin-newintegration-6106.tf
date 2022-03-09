ba77c8c79b652fe622fa51f7534401a4b5d4cc8b
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-03-08 16:00:31 -0800

    chore: remove a client file for junmin-newintegration-6106

--- terraform/keycloak-dev/realms/onestopauth/client-junmin-newintegration-6106.tf
+++
@@ -1,9 +0,0 @@
-module "client_junmin-newintegration-6106" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "junmin-newintegration-6106"
-  valid_redirect_uris = [
-    "http://localhost:3000"
-  ]
-  description = "CSS App Created"
-}
