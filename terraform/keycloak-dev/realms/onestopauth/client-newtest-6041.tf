f93b0e4f977c7dece61870825cfff001ecbae35d
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-03-10 09:34:17 -0800

    feat: add a client file for newtest-6041

---
+++ terraform/keycloak-dev/realms/onestopauth/client-newtest-6041.tf
@@ -0,0 +1,15 @@
+module "client_newtest-6041" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "newtest-6041"
+  valid_redirect_uris = [
+    "http://localhost:3000"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "http://localhost:3000",
+    "+"
+  ]
+}
