79a4cd89c3b2151fa9db6ab1164a32fb2f071d17
Junmin Ahn <36021827+junminahn@users.noreply.github.com>
2022-03-11 16:18:34 -0800

    chore: remove a client file for march-10-test-2-zorin-6613

--- terraform/keycloak-dev/realms/onestopauth-basic/client-march-10-test-2-zorin-6613.tf
+++
@@ -1,9 +0,0 @@
-module "client_march-10-test-2-zorin-6613" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "march-10-test-2-zorin-6613"
-  valid_redirect_uris = [
-    "https:///www.test.ca/devChange2"
-  ]
-  description = "CSS App Created"
-}
