a09aa1a1ba8c2fe1aa62a2aff0618d1b2347d80c
Junmin Ahn <36021827+junminahn@users.noreply.github.com>
2022-03-14 14:22:19 -0700

    feat: add a client file for tians-testing-b-ce-id-dev-test-and-prod-6735

---
+++ terraform/keycloak-prod/realms/onestopauth-basic/client-tians-testing-b-ce-id-dev-test-and-prod-6735.tf
@@ -0,0 +1,15 @@
+module "client_tians-testing-b-ce-id-dev-test-and-prod-6735" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "tians-testing-b-ce-id-dev-test-and-prod-6735"
+  valid_redirect_uris = [
+    "https://exampleProd"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://exampleProd",
+    "+"
+  ]
+}
