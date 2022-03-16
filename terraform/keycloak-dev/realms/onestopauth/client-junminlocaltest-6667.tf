f65d5599cb6082cbef341f02831c40d673ccb0cd
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-03-16 13:42:11 -0700

    chore: remove a client file for junminlocaltest-6667

--- terraform/keycloak-dev/realms/onestopauth/client-junminlocaltest-6667.tf
+++
@@ -1,9 +0,0 @@
-module "client_junminlocaltest-6667" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "junminlocaltest-6667"
-  valid_redirect_uris = [
-    "http://localhost:3000"
-  ]
-  description = "CSS App Created"
-}
