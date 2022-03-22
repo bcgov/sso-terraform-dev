7a3533365141366631ffa6e889c4876f614e2a6c
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-03-22 12:30:14 -0700

    chore: remove a client file for tians-testing-multiple-members-3-8-6403

--- terraform/keycloak-dev/realms/onestopauth/client-tians-testing-multiple-members-3-8-6403.tf
+++
@@ -1,9 +0,0 @@
-module "client_tians-testing-multiple-members-3-8-6403" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "tians-testing-multiple-members-3-8-6403"
-  valid_redirect_uris = [
-    "https://exampledfsasd"
-  ]
-  description = "CSS App Created"
-}
