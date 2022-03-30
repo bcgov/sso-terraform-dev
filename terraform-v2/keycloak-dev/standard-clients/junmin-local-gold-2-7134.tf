1b61ebb4366a8be7d20bdf213b901da0914dc349
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-03-30 12:08:52 -0700

    feat: add a client file for junmin-local-gold-2-7134

---
+++ terraform-v2/keycloak-dev/standard-clients/junmin-local-gold-2-7134.tf
@@ -0,0 +1,18 @@
+module "junmin-local-gold-2-7134" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = var.standard_realm_id
+  client_name = "junmin-local-gold-2-7134"
+  valid_redirect_uris = [
+    "http://localhost:3011"
+  ]
+  idps = [
+    "idir"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "http://localhost:3011",
+    "+"
+  ]
+}
