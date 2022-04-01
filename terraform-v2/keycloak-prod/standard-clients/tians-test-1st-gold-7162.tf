8689d77e647c28a962ecb1425a4744fa8f21b23b
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-01 10:18:15 -0700

    feat: add a client file for tians-test-1st-gold-7162

---
+++ terraform-v2/keycloak-prod/standard-clients/tians-test-1st-gold-7162.tf
@@ -0,0 +1,21 @@
+module "tians-test-1st-gold-7162" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = var.standard_realm_id
+  client_name = "tians-test-1st-gold-7162"
+  valid_redirect_uris = [
+    "https://exampleProd"
+  ]
+  idps = [
+    "idir",
+    "azureidir",
+    "bceidbusiness",
+    "bceidboth"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://exampleProd",
+    "+"
+  ]
+}
