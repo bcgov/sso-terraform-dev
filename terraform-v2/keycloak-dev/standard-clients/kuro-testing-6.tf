267cdeee91e333e18e97020b8dab0b211341f916
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-04 13:12:52 -0700

    feat: add a client file for kuro-testing-6

---
+++ terraform-v2/keycloak-dev/standard-clients/kuro-testing-6.tf
@@ -0,0 +1,19 @@
+module "kuro-testing-6" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = var.standard_realm_id
+  client_name = "kuro-testing-6"
+  valid_redirect_uris = [
+    "http://test.com"
+  ]
+  idps = [
+    "idir",
+    "bceidbasic"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "http://test.com",
+    "+"
+  ]
+}
