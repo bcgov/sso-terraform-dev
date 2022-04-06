b24fec491ee184198471c84f7bf9699bae64bff0
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-06 09:57:38 -0700

    feat: add a client file for junmin-test-329-7131

---
+++ terraform/keycloak-dev/realms/onestopauth/client-junmin-test-329-7131.tf
@@ -0,0 +1,15 @@
+module "client_junmin-test-329-7131" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "junmin-test-329-7131"
+  valid_redirect_uris = [
+    "http://localhost3030"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "http://localhost3030",
+    "+"
+  ]
+}
