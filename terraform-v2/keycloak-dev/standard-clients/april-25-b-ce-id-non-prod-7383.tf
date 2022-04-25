93ade57770c2d83dd3bb52fc392e1e600a7756b1
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-25 13:36:05 -0700

    feat: add a client file for april-25-b-ce-id-non-prod-7383

---
+++ terraform-v2/keycloak-dev/standard-clients/april-25-b-ce-id-non-prod-7383.tf
@@ -0,0 +1,26 @@
+module "april-25-b-ce-id-non-prod-7383" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = var.standard_realm_id
+  client_name = "april-25-b-ce-id-non-prod-7383"
+  valid_redirect_uris = [
+    "https:///www.test.ca/devChange",
+    "https:///www.test.ca/devChange56"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "bceidbasic",
+    "common"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https:///www.test.ca/devChange",
+    "https:///www.test.ca/devChange56",
+    "+"
+  ]
+}
