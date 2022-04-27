dba2c952345a2debc465145d4cbfc3a6a36d3ca2
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-27 12:09:55 -0700

    feat: add a client file for tian-second-gold-7370

---
+++ terraform-v2/keycloak-test/standard-clients/tian-second-gold-7370.tf
@@ -0,0 +1,26 @@
+data "keycloak_authentication_flow" "tian_second_gold_7370_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "tian-second-gold-7370" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = var.standard_realm_id
+  client_name = "tian-second-gold-7370"
+  valid_redirect_uris = [
+    "https://exampleTest"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.tian_second_gold_7370_browserflow.id
+}
