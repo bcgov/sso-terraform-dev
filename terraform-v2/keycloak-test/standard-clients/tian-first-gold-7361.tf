976f4672592b48269c39e84124444761ae1f3803
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-27 15:49:16 -0700

    feat: add a client file for tian-first-gold-7361

---
+++ terraform-v2/keycloak-test/standard-clients/tian-first-gold-7361.tf
@@ -0,0 +1,34 @@
+data "keycloak_authentication_flow" "tian_first_gold_7361_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "tian-first-gold-7361" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = var.standard_realm_id
+  client_name = "tian-first-gold-7361"
+  valid_redirect_uris = [
+    "https://exampleddd"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "bceidbasic",
+    "bceidbusiness",
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.tian_first_gold_7361_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://exampleddd",
+    "+"
+  ]
+}
