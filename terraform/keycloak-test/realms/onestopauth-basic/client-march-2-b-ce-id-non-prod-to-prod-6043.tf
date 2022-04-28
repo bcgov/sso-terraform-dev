ed4eaa492f6b42a00cf6b2d36adfe3b94d4a8592
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-04-28 16:08:42 -0700

    chore: remove a client file for march-2-b-ce-id-non-prod-to-prod-6043

--- terraform/keycloak-test/realms/onestopauth-basic/client-march-2-b-ce-id-non-prod-to-prod-6043.tf
+++
@@ -1,9 +0,0 @@
-module "client_march-2-b-ce-id-non-prod-to-prod-6043" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "march-2-b-ce-id-non-prod-to-prod-6043"
-  valid_redirect_uris = [
-    "https:///www.test.ca/devChange123"
-  ]
-  description = "CSS App Created"
-}
