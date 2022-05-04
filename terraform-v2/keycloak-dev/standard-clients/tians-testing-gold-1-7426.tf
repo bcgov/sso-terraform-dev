a92d2cce8623a43d0b5f2ada81ed507c32202708
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-04 09:45:53 -0700

    feat: add a client file for tians-testing-gold-1-7426

---
+++ terraform-v2/keycloak-dev/standard-clients/tians-testing-gold-1-7426.tf
@@ -0,0 +1,31 @@
+data "keycloak_authentication_flow" "tians_testing_gold_1_7426_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "tians-testing-gold-1-7426" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = var.standard_realm_id
+  client_name = "tians-testing-gold-1-7426"
+  valid_redirect_uris = [
+    "https://exampleddd"
+  ]
+  access_token_lifespan               = 120
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbasic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.tians_testing_gold_1_7426_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://exampleddd",
+    "+"
+  ]
+}
