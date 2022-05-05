a60a2a434fb3f9e336c0df57ea859060ca8f840d
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-05 09:08:05 -0700

    chore: remove a client file for tians-testing-silver-in-silver-2-7380

--- terraform/keycloak-test/realms/onestopauth-basic/client-tians-testing-silver-in-silver-2-7380.tf
+++
@@ -1,9 +0,0 @@
-module "client_tians-testing-silver-in-silver-2-7380" {
-  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
-  realm_id    = data.keycloak_realm.this.id
-  client_name = "tians-testing-silver-in-silver-2-7380"
-  valid_redirect_uris = [
-    "https://exampleTest"
-  ]
-  description = "CSS App Created"
-}
