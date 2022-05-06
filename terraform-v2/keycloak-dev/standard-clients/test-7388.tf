1a63c2df1a9042d33c723a999474d3f4b3b46529
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-06 11:43:12 -0700

    feat: add a client file for test-7388

---
+++ terraform-v2/keycloak-dev/standard-clients/test-7388.tf
@@ -0,0 +1,31 @@
+data "keycloak_authentication_flow" "test_7388_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "test-7388" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = var.standard_realm_id
+  client_name = "test-7388"
+  valid_redirect_uris = [
+    "http://localhost:3000"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbasic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.test_7388_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:3000",
+    "+"
+  ]
+}
