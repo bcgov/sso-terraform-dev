519158daa6183028e1821482734b271b7ae5bb23
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-09 06:03:18 -0700

    feat: add a client file for kuro-testing-7440

---
+++ terraform/keycloak-test/realms/onestopauth-both/client-kuro-testing-7440.tf
@@ -0,0 +1,15 @@
+module "client_kuro-testing-7440" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_name = "kuro-testing-7440"
+  valid_redirect_uris = [
+    "http://testchange.com"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "http://testchange.com",
+    "+"
+  ]
+}
