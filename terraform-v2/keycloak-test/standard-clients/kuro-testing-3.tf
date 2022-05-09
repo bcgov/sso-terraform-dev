f3ae16ab530f730159c067d496ee6d0d04a2caf0
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-09 05:58:59 -0700

    feat: add a client file for kuro-testing-3

---
+++ terraform-v2/keycloak-test/standard-clients/kuro-testing-3.tf
@@ -0,0 +1,33 @@
+data "keycloak_authentication_flow" "kuro_testing_3_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "kuro-testing-3" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = var.standard_realm_id
+  client_name = "kuro-testing-3"
+  valid_redirect_uris = [
+    "http://testchange.com"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbasic",
+    "bceidbusiness",
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.kuro_testing_3_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://testchange.com",
+    "+"
+  ]
+}
