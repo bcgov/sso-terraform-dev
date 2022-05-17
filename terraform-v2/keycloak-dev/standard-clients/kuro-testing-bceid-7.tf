130319157481e05c8f41cfbbf454cbdfbf7ad1ab
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-17 00:36:43 -0700

    feat: add a client file for kuro-testing-bceid-7

---
+++ terraform-v2/keycloak-dev/standard-clients/kuro-testing-bceid-7.tf
@@ -0,0 +1,30 @@
+data "keycloak_authentication_flow" "kuro_testing_bceid_7_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "kuro-testing-bceid-7" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = var.standard_realm_id
+  client_name = "kuro-testing-bceid-7"
+  valid_redirect_uris = [
+    "http://test.com"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "bceidbasic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.kuro_testing_bceid_7_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://test.com",
+    "+"
+  ]
+}
