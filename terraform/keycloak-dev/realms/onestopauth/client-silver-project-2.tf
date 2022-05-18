abbbfbef6b57e09936ac1d9faa4fc8da0b2fbe28
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-18 16:14:18 -0700

    feat: add a client file for silver-project-2

---
+++ terraform/keycloak-dev/realms/onestopauth/client-silver-project-2.tf
@@ -0,0 +1,16 @@
+module "client_silver-project-2" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_id   = "silver-project-2"
+  client_name = "silver-project-2"
+  valid_redirect_uris = [
+    "http://localhost:3000"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "http://localhost:3000",
+    "+"
+  ]
+}
