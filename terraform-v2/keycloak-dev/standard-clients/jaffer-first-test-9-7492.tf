25254d44daa40f38c3f96012898ccb8e99431404
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-18 13:03:40 -0700

    feat: add a client file for jaffer-first-test-9-7492

---
+++ terraform-v2/keycloak-dev/standard-clients/jaffer-first-test-9-7492.tf
@@ -0,0 +1,30 @@
+data "keycloak_authentication_flow" "jaffer_first_test_9_7492_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "jaffer-first-test-9-7492" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = var.standard_realm_id
+  client_name = "jaffer-first-test-9-7492"
+  valid_redirect_uris = [
+    "https://localhost:8000"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.jaffer_first_test_9_7492_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://localhost:8000",
+    "+"
+  ]
+}
