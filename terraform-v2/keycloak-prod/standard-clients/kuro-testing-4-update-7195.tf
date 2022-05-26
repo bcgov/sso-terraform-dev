a07e7aee3b5dbbed1fa9be92901881696336d021
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-05-26 11:28:03 -0700

    feat: add a client file for kuro-testing-4-update-7195

---
+++ terraform-v2/keycloak-prod/standard-clients/kuro-testing-4-update-7195.tf
@@ -0,0 +1,31 @@
+data "keycloak_authentication_flow" "kuro_testing_4_update_7195_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "kuro-testing-4-update-7195" {
+  source      = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id    = var.standard_realm_id
+  client_id   = "kuro-testing-4-update-7195"
+  client_name = ""
+  valid_redirect_uris = [
+    "http://test.com"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "bceidbasic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.kuro_testing_4_update_7195_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://test.com",
+    "+"
+  ]
+}
