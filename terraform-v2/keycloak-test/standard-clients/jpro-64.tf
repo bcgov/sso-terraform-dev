0c26306e026a053a420f20d59a6e3de470e5f0d4
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-06-02 15:58:12 -0700

    feat: add a client file for jpro-64

---
+++ terraform-v2/keycloak-test/standard-clients/jpro-64.tf
@@ -0,0 +1,31 @@
+data "keycloak_authentication_flow" "jpro_64_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "jpro-64" {
+  source      = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id    = var.standard_realm_id
+  client_id   = "jpro-64"
+  client_name = "jpro"
+  valid_redirect_uris = [
+    "http://localhost:3000"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.jpro_64_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:3000",
+    "+"
+  ]
+}
