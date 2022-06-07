0d2e39ce4c8c6cb1b341f1762599da9faa488899
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-06-06 21:20:57 -0700

    feat: add a client file for may-19-zorin-test-idir-only-7468

---
+++ terraform-v2/keycloak-test/standard-clients/may-19-zorin-test-idir-only-7468.tf
@@ -0,0 +1,33 @@
+data "keycloak_authentication_flow" "may_19_zorin_test_idir_only_7468_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "may-19-zorin-test-idir-only-7468" {
+  source      = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id    = var.standard_realm_id
+  client_id   = "may-19-zorin-test-idir-only-7468"
+  client_name = "Default"
+  valid_redirect_uris = [
+    "http://localhost:3000",
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.may_19_zorin_test_idir_only_7468_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:3000",
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "+"
+  ]
+}
