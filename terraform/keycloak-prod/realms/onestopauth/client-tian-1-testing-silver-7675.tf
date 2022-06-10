9183a20c055bd2e9e288c8d8b1c2af8d60800b82
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-06-10 14:35:51 -0700

    feat: add a client file for tian-1-testing-silver-7675

---
+++ terraform/keycloak-prod/realms/onestopauth/client-tian-1-testing-silver-7675.tf
@@ -0,0 +1,16 @@
+module "client_tian-1-testing-silver-7675" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_id   = "tian-1-testing-silver-7675"
+  client_name = "tian-1-testing-silver-7675"
+  valid_redirect_uris = [
+    "https://exampleProd"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://exampleProd",
+    "+"
+  ]
+}
