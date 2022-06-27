4d4a5f7c6c9d6785bddf20fd02a3bcc9dc36a153
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-06-27 15:18:23 -0700

    feat: add a client file for march-4-test-2-6338

---
+++ terraform/keycloak-prod/realms/onestopauth-business/client-march-4-test-2-6338.tf
@@ -0,0 +1,16 @@
+module "client_march-4-test-2-6338" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_id   = "march-4-test-2-6338"
+  client_name = "march-4-test-2-6338"
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "+"
+  ]
+}
