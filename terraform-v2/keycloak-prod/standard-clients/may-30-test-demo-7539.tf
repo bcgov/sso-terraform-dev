ebe1c0c56ee5faa48437ed9fc5cbc9c575ababe7
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-07-11 09:31:35 -0700

    feat: add a client file for may-30-test-demo-7539

---
+++ terraform-v2/keycloak-prod/standard-clients/may-30-test-demo-7539.tf
@@ -0,0 +1,35 @@
+data "keycloak_authentication_flow" "may_30_test_demo_7539_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "may-30-test-demo-7539" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "may-30-test-demo-7539"
+  client_name                         = "May 30 Test Demo"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "bceidbusiness",
+    "bceidbasic",
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.may_30_test_demo_7539_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+}
