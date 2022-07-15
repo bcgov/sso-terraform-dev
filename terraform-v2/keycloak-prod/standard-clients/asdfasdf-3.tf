7d33c708f65b9432f026a68c171bb8da6ecec39f
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-07-15 16:41:22 -0700

    feat: add a client file for asdfasdf-3

---
+++ terraform-v2/keycloak-prod/standard-clients/asdfasdf-3.tf
@@ -0,0 +1,28 @@
+data "keycloak_authentication_flow" "asdfasdf_3_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "asdfasdf-3" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "asdfasdf-3"
+  client_name                         = "asdfasdf"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.asdfasdf_3_browserflow.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "http://localhost:2222"
+  ]
+}
