803952a4d2fa4c097c7545b0ad430ed5346cd79c
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-07-18 11:39:31 -0700

    feat: add a client file for confidential-example-app-integration-7801

---
+++ terraform/keycloak-dev/realms/onestopauth/client-confidential-example-app-integration-7801.tf
@@ -0,0 +1,10 @@
+module "client_confidential-example-app-integration-7801" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_id   = "confidential-example-app-integration-7801"
+  client_name = "confidential-example-app-integration-7801"
+  valid_redirect_uris = [
+    "http://localhost:3000"
+  ]
+  description = "CSS App Created"
+}
