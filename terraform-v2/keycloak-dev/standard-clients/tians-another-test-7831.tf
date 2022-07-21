145fa52db3ac951706b208c8165ecc02f402ab6f
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-07-21 15:44:27 -0700

    feat: add a client file for tians-another-test-7831

---
+++ terraform-v2/keycloak-dev/standard-clients/tians-another-test-7831.tf
@@ -0,0 +1,33 @@
+data "keycloak_authentication_flow" "tians_another_test_7831_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "tians-another-test-7831" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "tians-another-test-7831"
+  client_name                         = "Tian's another test"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "bceidbasic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.tians_another_test_7831_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://example",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://example"
+  ]
+}
