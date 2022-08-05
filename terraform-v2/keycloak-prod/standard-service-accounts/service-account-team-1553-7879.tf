c92f2b85567cdda1ae3c3dc90f24bd08dfdc477f
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-08-05 09:59:05 -0700

    feat: add a client file for service-account-team-1553-7879

---
+++ terraform-v2/keycloak-prod/standard-service-accounts/service-account-team-1553-7879.tf
@@ -0,0 +1,7 @@
+module "service-account-team-1553-7879" {
+  source      = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-service-account"
+  realm_id    = var.standard_realm_id
+  client_id   = "service-account-team-1553-7879"
+  team_id     = 1553
+  description = "CSS App Created"
+}
