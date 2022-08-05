04aad0ed6a8d54c9a8a65cd974e91ad49e4f3fb8
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-08-05 10:00:57 -0700

    feat: add a client file for junmin-integration-7821

---
+++ terraform-v2/keycloak-dev/standard-clients/junmin-integration-7821.tf
@@ -0,0 +1,35 @@
+data "keycloak_authentication_flow" "junmin_integration_7821_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "junmin-integration-7821" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "junmin-integration-7821"
+  client_name                         = "junmin integration2"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "bceidbasic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.junmin_integration_7821_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:3001",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://localhost:3001"
+  ]
+}
