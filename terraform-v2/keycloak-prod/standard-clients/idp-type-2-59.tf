1647e36c17c5101c34e83a16199ae4cd64e3c433
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-08-10 15:35:49 -0700

    feat: add a client file for idp-type-2-59

---
+++ terraform-v2/keycloak-prod/standard-clients/idp-type-2-59.tf
@@ -0,0 +1,33 @@
+data "keycloak_authentication_flow" "idp_type_2_59_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "idp-type-2-59" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "idp-type-2-59"
+  client_name                         = "idp_type2"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_type_2_59_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://test.com",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://test.com"
+  ]
+}
