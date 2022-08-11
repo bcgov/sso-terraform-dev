a46669e8df4899891a74b204afbc33759e12a634
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-08-11 08:36:04 -0700

    feat: add a client file for diff-idp-testing-7899

---
+++ terraform-v2/keycloak-dev/standard-clients/diff-idp-testing-7899.tf
@@ -0,0 +1,34 @@
+data "keycloak_authentication_flow" "diff_idp_testing_7899_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "diff-idp-testing-7899" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "diff-idp-testing-7899"
+  client_name                         = "diff_idp_testing"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "bceidbasic",
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.diff_idp_testing_7899_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://test.com",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://test.com"
+  ]
+}
