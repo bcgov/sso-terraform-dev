f3693e2071a867a55dcbd1ee36df869b5e716f5c
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-08-12 11:34:42 -0700

    chore: remove a client file for service-account-team-1859-7878

--- terraform-v2/keycloak-prod/standard-service-accounts/service-account-team-1859-7878.tf
+++
@@ -1,7 +0,0 @@
-module "service-account-team-1859-7878" {
-  source      = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-service-account"
-  realm_id    = var.standard_realm_id
-  client_id   = "service-account-team-1859-7878"
-  team_id     = 1859
-  description = "CSS App Created"
-}
