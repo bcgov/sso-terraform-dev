74071701310555ef73a543cb7d1b64aa6b502915
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-08-16 03:29:41 -0700

    feat: add a client file for idp-final-87

---
+++ terraform-v2/keycloak-dev/standard-clients/idp-final-87.tf
@@ -0,0 +1,36 @@
+data "keycloak_authentication_flow" "idp_final_87_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "idp-final-87" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "idp-final-87"
+  client_name                         = "idp_final"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "bceidbasic",
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_final_87_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://test2.com",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://test2.com"
+  ]
+}
