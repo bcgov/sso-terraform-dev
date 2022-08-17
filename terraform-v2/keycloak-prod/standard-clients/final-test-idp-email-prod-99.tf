fd7272d8ac0616baee59fb0378a542dd9c0ba4f0
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-08-16 21:54:22 -0700

    feat: add a client file for final-test-idp-email-prod-99

---
+++ terraform-v2/keycloak-prod/standard-clients/final-test-idp-email-prod-99.tf
@@ -0,0 +1,33 @@
+data "keycloak_authentication_flow" "final_test_idp_email_prod_99_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "final-test-idp-email-prod-99" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "final-test-idp-email-prod-99"
+  client_name                         = "final_test_idp_email_prod"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.final_test_idp_email_prod_99_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://test.com",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://test.com"
+  ]
+}
