2d26979eb1a9e22e5ab979dd3cd6ebe84ee32381
junmin-bcgov <85124898+junmin-bcgov@users.noreply.github.com>
2022-09-02 10:40:31 -0700

    feat: add a client file for tians-testing-silver-in-silver-t-t-7999

---
+++ terraform/keycloak-dev/realms/onestopauth/client-tians-testing-silver-in-silver-t-t-7999.tf
@@ -0,0 +1,16 @@
+module "client_tians-testing-silver-in-silver-t-t-7999" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_id   = "tians-testing-silver-in-silver-t-t-7999"
+  client_name = "tians-testing-silver-in-silver-t-t-7999"
+  valid_redirect_uris = [
+    "https://examplehttps://bcgov.github.io/keycloak-example-apps/"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://examplehttps://bcgov.github.io/keycloak-example-apps/",
+    "+"
+  ]
+}
