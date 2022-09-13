6fbb9b4815a3f4d0b7f0aa680e667be73b8437b9
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-09-13 11:02:18 -0700

    feat: add a client file for tians-testing-git-hub-oidc-public-8037

---
+++ terraform-v2/keycloak-test/standard-clients/tians-testing-git-hub-oidc-public-8037.tf
@@ -0,0 +1,36 @@
+data "keycloak_authentication_flow" "tians_testing_git_hub_oidc_public_8037_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "tians-testing-git-hub-oidc-public-8037" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "tians-testing-git-hub-oidc-public-8037"
+  client_name                         = "Tian's Testing - GitHub OIDC Public"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "bceidboth",
+    "github",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.tians_testing_git_hub_oidc_public_8037_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+}
