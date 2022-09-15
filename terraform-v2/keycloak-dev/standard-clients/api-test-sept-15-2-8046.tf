c5575bf5aa61dc7b47590f2c12e39126716e7cb8
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-09-15 10:16:28 -0700

    feat: add a client file for api-test-sept-15-2-8046

---
+++ terraform-v2/keycloak-dev/standard-clients/api-test-sept-15-2-8046.tf
@@ -0,0 +1,33 @@
+data "keycloak_authentication_flow" "api_test_sept_15_2_8046_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "api-test-sept-15-2-8046" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "api-test-sept-15-2-8046"
+  client_name                         = "Api Test Sept 15#2"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.api_test_sept_15_2_8046_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+}
