04e5c2c6e5d67bbaedbc218c440883694e0c2a0d
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-09-15 09:59:59 -0700

    feat: add a client file for tians-testing-github-saml-8045

---
+++ terraform-v2/keycloak-prod/standard-clients/tians-testing-github-saml-8045.tf
@@ -0,0 +1,22 @@
+data "keycloak_authentication_flow" "tians_testing_github_saml_8045_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "tians-testing-github-saml-8045" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "tians-testing-github-saml-8045"
+  client_name        = "Tian's Testing - Github SAML"
+  assertion_lifespan = 1920
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+  idps = [
+    "idir",
+    "azureidir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.tians_testing_github_saml_8045_browserflow.id
+}
