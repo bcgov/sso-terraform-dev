43b964eb41dd7fa14349b7f1e7b1677d354880fe
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-09-16 17:33:05 -0700

    feat: add a client file for tians-testing-github-b-ce-id-prod-approval-8060

---
+++ terraform-v2/keycloak-prod/standard-clients/tians-testing-github-b-ce-id-prod-approval-8060.tf
@@ -0,0 +1,36 @@
+data "keycloak_authentication_flow" "tians_testing_github_b_ce_id_prod_approval_8060_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "tians-testing-github-b-ce-id-prod-approval-8060" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "tians-testing-github-b-ce-id-prod-approval-8060"
+  client_name                         = "Tian's Testing - Github BCeID Prod Approval"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "bceidbasic",
+    "bceidbusiness",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.tians_testing_github_b_ce_id_prod_approval_8060_browserflow.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://exampleProd",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://exampleProd"
+  ]
+}
