f59f2c511ff0c4c72e17e7d8360673f2f7298107
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-09-21 09:07:38 -0700

    feat: add a client file for tians-testing-service-account-8080

---
+++ terraform-v2/keycloak-dev/standard-clients/tians-testing-service-account-8080.tf
@@ -0,0 +1,23 @@
+data "keycloak_authentication_flow" "tians_testing_service_account_8080_browserflow" {
+  realm_id = var.standard_realm_id
+  alias    = "idp stopper"
+}
+module "tians-testing-service-account-8080" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "tians-testing-service-account-8080"
+  client_name                         = "Tian's Testing - service account"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "common"
+  ]
+  description                  = "CSS App Created"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.tians_testing_service_account_8080_browserflow.id
+  standard_flow_enabled        = false
+  service_accounts_enabled     = true
+}
