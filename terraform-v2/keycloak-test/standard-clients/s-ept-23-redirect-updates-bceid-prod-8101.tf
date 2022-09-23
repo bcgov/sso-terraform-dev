13f4563d4adb2c6171d4e0bce6e5e4447a24c2d2
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-09-23 15:33:03 -0700

    feat: add a client file for s-ept-23-redirect-updates-bceid-prod-8101

---
+++ terraform-v2/keycloak-test/standard-clients/s-ept-23-redirect-updates-bceid-prod-8101.tf
@@ -0,0 +1,32 @@
+module "s-ept-23-redirect-updates-bceid-prod-8101" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "s-ept-23-redirect-updates-bceid-prod-8101"
+  client_name                         = "SEpt 23 - redirect updates bceid prod"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "bceidbasic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "https://www.test.ca/testCHANGED2",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "https://www.test.ca/testCHANGED2"
+  ]
+}
