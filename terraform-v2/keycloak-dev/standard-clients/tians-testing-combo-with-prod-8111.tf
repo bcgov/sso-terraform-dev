82a69ef1b7f7b5c890a6175fa61ad56c48a7a3c8
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-09-26 16:04:15 -0700

    feat: add a client file for tians-testing-combo-with-prod-8111

---
+++ terraform-v2/keycloak-dev/standard-clients/tians-testing-combo-with-prod-8111.tf
@@ -0,0 +1,34 @@
+module "tians-testing-combo-with-prod-8111" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "tians-testing-combo-with-prod-8111"
+  client_name                         = "Tian's Testing - combo with prod"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "bceidbasic",
+    "bceidbusiness",
+    "github",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://example",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://example"
+  ]
+}
