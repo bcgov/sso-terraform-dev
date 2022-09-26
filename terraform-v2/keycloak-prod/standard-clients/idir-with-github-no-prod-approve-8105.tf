a25b825d4bb817f5052ecc924f8de793089fba97
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-09-26 10:48:35 -0700

    feat: add a client file for idir-with-github-no-prod-approve-8105

---
+++ terraform-v2/keycloak-prod/standard-clients/idir-with-github-no-prod-approve-8105.tf
@@ -0,0 +1,30 @@
+module "idir-with-github-no-prod-approve-8105" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "idir-with-github-no-prod-approve-8105"
+  client_name                         = "IDIR with Github no prod approve"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://githubprod.com",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://githubprod.com"
+  ]
+}
