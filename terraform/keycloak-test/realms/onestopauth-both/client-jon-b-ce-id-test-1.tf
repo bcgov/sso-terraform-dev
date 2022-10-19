933c06672be08fe9088c9b09172274da21c11e15
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-10-19 15:48:02 -0700

    feat: add a client file for jon-b-ce-id-test-1

---
+++ terraform/keycloak-test/realms/onestopauth-both/client-jon-b-ce-id-test-1.tf
@@ -0,0 +1,16 @@
+module "client_jon-b-ce-id-test-1" {
+  source      = "github.com/bcgov/sso-terraform-keycloak-client?ref=dev"
+  realm_id    = data.keycloak_realm.this.id
+  client_id   = "jon-b-ce-id-test-1"
+  client_name = "jon-b-ce-id-test-1"
+  valid_redirect_uris = [
+    "https://test.com"
+  ]
+  description                = "CSS App Created"
+  access_type                = "PUBLIC"
+  pkce_code_challenge_method = "S256"
+  web_origins = [
+    "https://test.com",
+    "+"
+  ]
+}
