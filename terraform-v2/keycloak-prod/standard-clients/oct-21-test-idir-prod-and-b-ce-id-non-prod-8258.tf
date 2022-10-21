87ea18e99ff8936e41d1024194ed3a70686f78b0
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-10-21 16:07:35 -0700

    feat: add a client file for oct-21-test-idir-prod-and-b-ce-id-non-prod-8258

---
+++ terraform-v2/keycloak-prod/standard-clients/oct-21-test-idir-prod-and-b-ce-id-non-prod-8258.tf
@@ -0,0 +1,30 @@
+module "oct-21-test-idir-prod-and-b-ce-id-non-prod-8258" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "oct-21-test-idir-prod-and-b-ce-id-non-prod-8258"
+  client_name                         = "oct 21 test IDIR PROD and BCeID Non PROD"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+}
