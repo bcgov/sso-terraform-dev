238155ec53d88182b8a2784b3fe95a0c55d50f95
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-10-24 16:21:50 -0700

    feat: add a client file for auth-type-test-109

---
+++ terraform-v2/keycloak-test/standard-clients/auth-type-test-109.tf
@@ -0,0 +1,24 @@
+module "auth-type-test-109" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "auth-type-test-109"
+  client_name                         = "authType_test"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "http://test.com"
+  ]
+}
