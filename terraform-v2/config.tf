a5445244d8934a74945c020da907ec17a4fe3f2d
Junmin Ahn <junminahn@outlook.com>
2022-11-21 08:51:15 -0800

    chore: update terraform v2 config

--- terraform-v2/config.tf
+++ terraform-v2/config.tf
@@ -2,9 +2,10 @@ terraform {
   required_version = ">= 0.15.5"
 
   backend "s3" {
-    bucket = "xgr00q-dev-keycloak"
-    key    = "keycloak/gold"
-    region = "ca-central-1"
+    bucket         = "xgr00q-dev-keycloak"
+    key            = "keycloak/gold"
+    dynamodb_table = "xgr00q-dev-state-locking"
+    region         = "ca-central-1"
   }
 
   required_providers {
