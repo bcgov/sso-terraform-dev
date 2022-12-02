095569cb17ce8a77afcd312f7b8bb424e009db96
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-01 16:03:27 -0800

    feat: add a client file for test-with-junmin-8319

---
+++ terraform-v2/keycloak-dev/standard-clients/test-with-junmin-8319.tf
@@ -0,0 +1,25 @@
+module "test-with-junmin-8319" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "test-with-junmin-8319"
+  client_name                         = "Test with Junmin2"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbasic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps2/*"
+  ]
+}
