3166446faf6a5cdee62ff34175fc85d382d0bf00
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-02 14:17:13 -0800

    feat: add a client file for test-service-account-idir-nk-8333

---
+++ terraform-v2/keycloak-dev/standard-clients/test-service-account-idir-nk-8333.tf
@@ -0,0 +1,21 @@
+module "test-service-account-idir-nk-8333" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "test-service-account-idir-nk-8333"
+  client_name                         = "test-service-account-idir-nk"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = false
+  service_accounts_enabled     = true
+}
