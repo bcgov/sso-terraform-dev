904b8757317d2f1552b1fd5fe9b4ad4d6309135a
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-16 15:09:30 -0800

    feat: add a client file for gs-test-2-8353

---
+++ terraform-v2/keycloak-dev/standard-clients/gs-test-2-8353.tf
@@ -0,0 +1,28 @@
+module "gs-test-2-8353" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "gs-test-2-8353"
+  client_name                         = "GS - Test 2"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "bceidboth",
+    "githubbcgov",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = "govind_role"
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+}
