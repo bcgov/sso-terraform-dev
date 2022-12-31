4e77c0224ed44af53c5277555d64e3162672f989
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2022-12-30 22:06:04 -0800

    feat: add a client file for css-api-k-6-load-testing-8417

---
+++ terraform-v2/keycloak-dev/standard-clients/css-api-k-6-load-testing-8417.tf
@@ -0,0 +1,31 @@
+module "css-api-k-6-load-testing-8417" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "css-api-k-6-load-testing-8417"
+  client_name                         = "CSS API K6 Load Testing"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://example",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://example"
+  ]
+}
