d665404b1b8ae3465682a6cdb35fea7eab11c897
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-03-21 08:20:47 -0700

    feat: add a client file for ui-testing-my-teams-panel-8442

---
+++ terraform-v2/keycloak-test/standard-clients/ui-testing-my-teams-panel-8442.tf
@@ -0,0 +1,27 @@
+module "ui-testing-my-teams-panel-8442" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "ui-testing-my-teams-panel-8442"
+  client_name                         = "UI_Testing_My_Teams_Panel"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbasic",
+    "githubpublic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://test.com"
+  ]
+}
