887d3d963c73c69668405e1961c2404946d6862b
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-03-22 10:08:44 -0700

    feat: add a client file for test-saml-logout-nk-1-8555

---
+++ terraform-v2/keycloak-test/standard-clients/test-saml-logout-nk-1-8555.tf
@@ -0,0 +1,19 @@
+module "test-saml-logout-nk-1-8555" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "test-saml-logout-nk-1-8555"
+  client_name        = "test-saml-logout-nk1"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "*"
+  ]
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  logout_post_binding_url      = "http://localhost:8080/logout/callback"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
