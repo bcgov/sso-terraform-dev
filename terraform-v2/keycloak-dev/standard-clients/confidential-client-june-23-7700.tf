562a75f649221d11f3fa2e2cd2bae1aa82d80311
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-03-29 09:54:57 -0700

    feat: add a client file for confidential-client-june-23-7700

---
+++ terraform-v2/keycloak-dev/standard-clients/confidential-client-june-23-7700.tf
@@ -0,0 +1,26 @@
+module "confidential-client-june-23-7700" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "confidential-client-june-23-7700"
+  client_name                         = "confidential client June 23"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://localhost:3000",
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+}
