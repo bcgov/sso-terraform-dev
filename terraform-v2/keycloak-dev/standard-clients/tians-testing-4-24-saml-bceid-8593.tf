5517847e63a9d003421a0294a3b610715590d4a8
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-04-24 16:13:13 -0700

    feat: add a client file for tians-testing-4-24-saml-bceid-8593

---
+++ terraform-v2/keycloak-dev/standard-clients/tians-testing-4-24-saml-bceid-8593.tf
@@ -0,0 +1,19 @@
+module "tians-testing-4-24-saml-bceid-8593" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "Tian-SAML-OverRide-update"
+  client_name        = "Tian's Testing - 4/24 Saml bceid"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+  idps = [
+    "bceidbasic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  logout_post_binding_url      = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
