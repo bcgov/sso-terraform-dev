6ad40709405c5f26bf17050a00f4033126dd4ad1
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-05-04 21:30:15 -0700

    feat: add a client file for may-1-saml-test-8604

---
+++ terraform-v2/keycloak-dev/standard-clients/may-1-saml-test-8604.tf
@@ -0,0 +1,20 @@
+module "may-1-saml-test-8604" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "zorinchangedmay42100hrs"
+  client_name        = "May 1 SAML Test"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/",
+    "https://localhost:3000/*"
+  ]
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  logout_post_binding_url      = "https://bcgov.github.io/keycloak-example-apps/"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
