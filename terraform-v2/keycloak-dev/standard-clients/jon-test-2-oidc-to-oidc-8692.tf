81ee82d72e50fc7cdf39aaf02229ea7349ea81ee
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-05-04 21:17:23 -0700

    feat: add a client file for jon-test-2-oidc-to-oidc-8692

---
+++ terraform-v2/keycloak-dev/standard-clients/jon-test-2-oidc-to-oidc-8692.tf
@@ -0,0 +1,25 @@
+module "jon-test-2-oidc-to-oidc-8692" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "jon-test-2-oidc-to-oidc-8692"
+  client_name                         = "Jon test 2 oidc to oidc"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = false
+  valid_redirect_uris = [
+    "http://localhost:3000/oauth/cognito"
+  ]
+}
