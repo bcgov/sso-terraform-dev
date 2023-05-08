da04793eb10d12a394ae094370f084516e2da885
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-05-08 10:32:13 -0700

    feat: add a client file for may-4-saml-zorin-c-8690

---
+++ terraform-v2/keycloak-dev/standard-clients/may-4-saml-zorin-c-8690.tf
@@ -0,0 +1,20 @@
+module "may-4-saml-zorin-c-8690" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "may-4-saml-zorin-c-8690"
+  client_name        = "May 4 OIDC Zorin"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "http://localhost:3000/*",
+    "https://bcgov.github.io/keycloak-example-apps/"
+  ]
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  logout_post_binding_url      = "https://bcgov.github.io/keycloak-example-apps/"
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
