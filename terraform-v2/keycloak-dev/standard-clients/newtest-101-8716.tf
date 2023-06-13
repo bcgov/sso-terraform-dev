82a31ab8bb936f78e48649f007446228a17b3f60
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-06-13 14:00:11 -0700

    feat: add a client file for newtest-101-8716

---
+++ terraform-v2/keycloak-dev/standard-clients/newtest-101-8716.tf
@@ -0,0 +1,25 @@
+module "newtest-101-8716" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "newtest-101-8716"
+  client_name                         = "Newtest101"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "http://Newtest101.com"
+  ]
+}
