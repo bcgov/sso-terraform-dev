5d7beb7343da29a20da2ab642e34e9e25c2abd62
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-06-30 09:33:00 -0700

    feat: add a client file for kuro-test-team-id-issue-22-8741

---
+++ terraform-v2/keycloak-dev/standard-clients/kuro-test-team-id-issue-2222-8741.tf
@@ -0,0 +1,32 @@
+module "kuro-test-team-id-issue-2222-8741" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "kuro-test-team-id-issue-2222-8741"
+  client_name                         = "kuro-test-teamId-issue-2222"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "bceidbasic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:3000",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://localhost:3000"
+  ]
+}
