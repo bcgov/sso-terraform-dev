518048ee69a181333924db41426cd253e2b076ff
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-07-05 14:11:48 -0700

    feat: add a client file for git-hub-public-june-2023-8727

---
+++ terraform-v2/keycloak-test/standard-clients/git-hub-public-june-2023-8727.tf
@@ -0,0 +1,33 @@
+module "git-hub-public-june-2023-8727" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "git-hub-public-june-2023-8727"
+  client_name                         = "GitHub Public June 2023"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "githubpublic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "http://localhost:3000/*",
+    "http://localhost:3500/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "http://localhost:3000/*",
+    "http://localhost:3500/*"
+  ]
+}
