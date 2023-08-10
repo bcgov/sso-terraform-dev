220ad76201dc898c30d70a3b35d90368e29dd07d
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-08-10 14:42:17 -0700

    feat: add a client file for tians-testing-8-10-email-notification-8817

---
+++ terraform-v2/keycloak-dev/standard-clients/tians-testing-8-10-email-notification-8817.tf
@@ -0,0 +1,34 @@
+module "tians-testing-8-10-email-notification-8817" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "tians-testing-8-10-email-notification-8817"
+  client_name                         = "Tian's Testing - 8/10 email notification"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "azureidir",
+    "bceidbasic",
+    "githubpublic",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://bcgov.github.io/keycloak-example-apps/*",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+}
