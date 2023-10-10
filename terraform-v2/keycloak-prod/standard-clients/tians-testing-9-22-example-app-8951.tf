bd91c57563896a0b4a6a7c707394c8c7e200ee6a
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-10-10 09:42:50 -0700

    feat: add a client file for tians-testing-9-22-example-app-8951

---
+++ terraform-v2/keycloak-prod/standard-clients/tians-testing-9-22-example-app-8951.tf
@@ -0,0 +1,26 @@
+module "tians-testing-9-22-example-app-8951" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "tians-testing-9-22-example-app-8951"
+  client_name                         = "Tian's Testing 9/22 - example app"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  valid_redirect_uris = [
+    "http://localhost:3000/*",
+    "https://bcgov.github.io/keycloak-example-apps/*"
+  ]
+}
