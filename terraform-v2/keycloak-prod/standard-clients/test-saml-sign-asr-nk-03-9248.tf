05ddaf8f0f51814e517f58895432ef62f64162b0
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-10-17 10:34:43 -0700

    feat: add a client file for test-saml-sign-asr-nk-03-9248

---
+++ terraform-v2/keycloak-prod/standard-clients/test-saml-sign-asr-nk-03-9248.tf
@@ -0,0 +1,20 @@
+module "test-saml-sign-asr-nk-03-9248" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "test-saml-sign-asr-nk-03-9248"
+  client_name        = "test-saml-sign-asr-nk03"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "https://example.com"
+  ]
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  logout_post_binding_url      = ""
+  sign_assertions              = true
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
