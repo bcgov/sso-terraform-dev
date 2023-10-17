ea753a13ed97b2e09d499583e8e5350faec64e7b
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-10-17 12:02:17 -0700

    feat: add a client file for new-int-9249

---
+++ terraform-v2/keycloak-dev/standard-clients/new-int-9249.tf
@@ -0,0 +1,20 @@
+module "new-int-9249" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "new-int-9249"
+  client_name        = "New Int"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "http://localhost"
+  ]
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  logout_post_binding_url      = "http://localhost/logout"
+  sign_assertions              = false
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
