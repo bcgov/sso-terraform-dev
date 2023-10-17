5b5da36c37f315b43322750d9939d93498f0b9ab
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-10-17 15:17:53 -0700

    feat: add a client file for secured-dedicated-instruction-set-9256

---
+++ terraform-v2/keycloak-dev/standard-clients/secured-dedicated-instruction-set-9256.tf
@@ -0,0 +1,31 @@
+module "secured-dedicated-instruction-set-9256" {
+  source                              = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client"
+  realm_id                            = var.standard_realm_id
+  client_id                           = "secured-dedicated-instruction-set-9256"
+  client_name                         = "Secured dedicated instruction set"
+  access_token_lifespan               = ""
+  client_session_idle_timeout         = ""
+  client_session_max_lifespan         = ""
+  client_offline_session_idle_timeout = ""
+  client_offline_session_max_lifespan = ""
+  idps = [
+    "idir",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = ""
+  login_theme                  = ""
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+  access_type                  = "PUBLIC"
+  pkce_code_challenge_method   = "S256"
+  web_origins = [
+    "https://spiteful-pillar.biz",
+    "+"
+  ]
+  standard_flow_enabled    = true
+  service_accounts_enabled = false
+  valid_redirect_uris = [
+    "https://spiteful-pillar.biz"
+  ]
+}
