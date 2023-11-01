acb5d7dd985d12f2d27f91ebac18c7a417d60924
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-11-01 15:34:03 -0700

    feat: add a client file for create-saml-2-team-9592

---
+++ terraform-v2/keycloak-dev/standard-clients/create-saml-2-team-9592.tf
@@ -0,0 +1,20 @@
+module "create-saml-2-team-9592" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "create-saml-2-team-9592"
+  client_name        = "Header Shown Dev"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "https://dev.com"
+  ]
+  idps = [
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = "TBD"
+  logout_post_binding_url      = "*"
+  sign_assertions              = false
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
