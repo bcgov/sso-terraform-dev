56227cdf87ad6f7bd89164ef7cd4b8a83844ffa6
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-11-01 16:24:21 -0700

    feat: add a client file for create-saml-1-team-9591

---
+++ terraform-v2/keycloak-test/standard-clients/create-saml-1-team-9591.tf
@@ -0,0 +1,20 @@
+module "create-saml-1-team-9591" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "create-saml-1-team-9591"
+  client_name        = "Create SAML 1 Team"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "https://test.com"
+  ]
+  idps = [
+    "bceidboth",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = "TBD"
+  logout_post_binding_url      = ""
+  sign_assertions              = false
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
