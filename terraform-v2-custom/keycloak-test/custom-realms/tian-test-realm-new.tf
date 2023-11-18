fef49f07a074ecb1040d115c7e91ae2f69bf875e
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2023-11-18 12:12:10 -0800

    feat: update tian test realm to new

--- terraform-v2-custom/keycloak-test/custom-realms/tian-test-realm-new.tf
+++ terraform-v2-custom/keycloak-test/custom-realms/tian-test-realm-new.tf
@@ -1,5 +1,5 @@
 module "tian-test-realm-new" {
   source     = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/custom-realm"
-  realm_name = "tian-test-realm"
+  realm_name = "tian-test-realm-new"
   enabled    = true
 }
