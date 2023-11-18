11bf634e712d2a1579bc575ca36ca9efefb58975
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2023-11-18 12:04:34 -0800

    feat: tf apply continue on error

--- terraform-v2-custom/keycloak-test/custom-realms/tian-test-realm-new.tf
+++ terraform-v2-custom/keycloak-test/custom-realms/tian-test-realm-new.tf
@@ -1,4 +1,4 @@
-module "tian-test-realm" {
+module "tian-test-realm-new" {
   source     = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/custom-realm"
   realm_name = "tian-test-realm"
   enabled    = true
