cbc883452675aaab867295b1af2690061d879b13
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2023-12-05 12:26:22 -0800

    feat: add a client file for role-test-saml-browser-login-public-git-hub-bc-gov-10152

---
+++ terraform-v2/keycloak-test/standard-clients/role-test-saml-browser-login-public-git-hub-bc-gov-10152.tf
@@ -0,0 +1,20 @@
+module "role-test-saml-browser-login-public-git-hub-bc-gov-10152" {
+  source             = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/standard-client-saml"
+  realm_id           = var.standard_realm_id
+  client_id          = "role-test-saml-browser-login-public-git-hub-bc-gov-10152"
+  client_name        = "SSO Header Test"
+  assertion_lifespan = ""
+  valid_redirect_uris = [
+    "*"
+  ]
+  idps = [
+    "githubbcgov",
+    "common"
+  ]
+  description                  = "CSS App Created"
+  additional_role_attribute    = "tbd"
+  logout_post_binding_url      = ""
+  sign_assertions              = false
+  override_authentication_flow = true
+  browser_authentication_flow  = data.keycloak_authentication_flow.idp_stopper.id
+}
