0b3edca8d71982fef5873091ea0100894d0b2452
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2024-01-05 16:44:00 -0800

    feat: create custom realm tian-test-realm-2024-01-05-a

---
+++ terraform-v2-custom/keycloak-dev/custom-realms/tian-test-realm-2024-01-05-a.tf
@@ -0,0 +1,5 @@
+module "tian-test-realm-2024-01-05-a" {
+  source     = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/custom-realm"
+  realm_name = "tian-test-realm-2024-01-05-a"
+  enabled    = true
+}
