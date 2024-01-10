d227e987089f8dd4d3e336d593e15bdf964534a7
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2024-01-10 09:40:44 -0800

    feat: use local admin user instead of service account

--- terraform-v2-custom/keycloak-test/provider.tf
+++ terraform-v2-custom/keycloak-test/provider.tf
@@ -1,8 +1,6 @@
 provider "keycloak" {
-  realm         = "master"
-  client_id     = var.client_id
-  client_secret = var.client_secret
-  username      = var.username
-  password      = var.password
-  url           = var.keycloak_url
+  client_id = "admin-cli"
+  username  = var.username
+  password  = var.password
+  url       = var.keycloak_url
 }
