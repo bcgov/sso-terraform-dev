fe17267adbdf5536caab1dfce548ae16b5b8a320
Pathfinder SSO Team <88675972+Pathfinder-SSO-Team@users.noreply.github.com>
2024-01-18 11:43:06 -0800

    feat: create custom realm tian-20240118-pr-success-edit-a

---
+++ terraform-v2-custom/keycloak-test/custom-realms/tian-20240118-pr-success-edit-a.tf
@@ -0,0 +1,5 @@
+module "tian-20240118-pr-success-edit-a" {
+  source     = "github.com/bcgov/sso-terraform-modules?ref=dev/modules/custom-realm"
+  realm_name = "tian-20240118-pr-success-edit-a"
+  enabled    = true
+}
