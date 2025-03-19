47717e88d62f23f262243d6a8abf2688091e2a8b
Jonathan Sharman <jon.w.sharman@gmail.com>
2025-03-19 15:04:20 -0700

    chore: add terraform config for google

--- terraform-v2/keycloak-dev/variables.tf
+++ terraform-v2/keycloak-dev/variables.tf
@@ -62,3 +62,12 @@ variable "digitalcredential_client_secret" {
   type      = string
   sensitive = true
 }
+# google and microsoft variables
+variable "google_client_id" {
+  type      = string
+  sensitive = true
+}
+variable "google_client_secret" {
+  type      = string
+  sensitive = true
+}
--- terraform-v2/keycloak-prod/variables.tf
+++ terraform-v2/keycloak-prod/variables.tf
@@ -53,6 +53,16 @@ variable "github_client_secret" {
   sensitive = true
 }
 
+variable "google_client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "google_client_secret" {
+  type      = string
+  sensitive = true
+}
+
 variable "digitalcredential_client_id" {
   type      = string
   sensitive = true
@@ -62,3 +72,4 @@ variable "digitalcredential_client_secret" {
   type      = string
   sensitive = true
 }
+
